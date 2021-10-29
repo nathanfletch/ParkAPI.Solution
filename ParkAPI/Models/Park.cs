c92aa060698142bc19469e7fcfb60229d74d6886
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-10-29 09:29:22 -0700

    Add initial files

---
+++ ParkAPI/Models/Park.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ParkAPI.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public int Score { get; set; }
+  }
+}
